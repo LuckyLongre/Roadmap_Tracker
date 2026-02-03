0c159470d81961009b964ca77da2ec99ce2cad71
LuckyLongre <sem2luckylongre@gmail.com>
2026-02-03 19:46:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -494,7 +494,7 @@ const PhaseListItem = ({ phase, index, isActive, onClick, onDelete }) => {
                      phase.root.getCompletionState() === 'partial' ? 'In Progress' : 'Not Started'}
                 </div>
             </div>
-                                
+{/*
             <button
                 className="phase-delete"
                 onClick={(e) => {
@@ -505,7 +505,7 @@ const PhaseListItem = ({ phase, index, isActive, onClick, onDelete }) => {
             >
                 <i className="fas fa-trash"></i>
             </button>
-            
+         */}
         </div>
     );
 };
